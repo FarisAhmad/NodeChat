3a66eb94b065933e1403d68482b80a7ce497df15
Pedro Henrique <pehfernandesl@gmail.com>
2019-01-16 09:40:43 -0200

    Formata o espaçamento da linha 43

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(expressSession({
 }));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 

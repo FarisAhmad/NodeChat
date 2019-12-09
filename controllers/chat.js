a1ac58ad439882458a2f13829ae5aff4338db53b
Pedro Henrique <pehfernandesl@gmail.com>
2019-12-09 20:42:11 -0300

    Formata o código de acordo com o prettier

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
-	var ChatController = {
-		index: function(req, res){
-			var params = {sala: req.query.sala};
-			res.render('chat/index', params);
-		}
-	};
-	return ChatController;
+  var ChatController = {
+    index: function(req, res) {
+      var params = { sala: req.query.sala };
+      res.render('chat/index', params);
+    },
+  };
+  return ChatController;
 };
